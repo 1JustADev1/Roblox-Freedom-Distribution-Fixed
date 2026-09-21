e454cc7f79dedac65154aa8c2fa48235ac512bfc
JustADev <kuklisolga461@gmail.com>
2026-09-21 14:24:48 +0400

    Update _main.py

--- Source/_main.py
+++ Source/_main.py
@@ -1,5 +1,11 @@
 import traceback
 import launcher
+import sys
+try:
+    sys.stdout.reconfigure(encoding="utf-8", errors="replace")
+    sys.stderr.reconfigure(encoding="utf-8", errors="replace")
+except Exception:
+    pass
 
 INTERRUPT_MESSAGE = '** RECEIVED Ctrl+C **'
 
